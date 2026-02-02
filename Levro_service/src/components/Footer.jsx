59c7e70e3604af98243c0988f00da3e5b09a0a60
ackermanlevro-dev <ackerman.levro@gmail.com>
2026-02-02 16:52:52 +0530

    Added footer component to our website

---
+++ Levro_service/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: "40px", textAlign: "center", opacity: 0.6 }}>
+      © 2026 IT Service Co. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
