571be2e42a9a3b39003ceb88c31ade87be14511a
HARIHARAN26424 <146554348+HARIHARAN26424@users.noreply.github.com>
2026-02-05 23:31:35 +0530

    first commit of hari - about us

--- Levro_service/src/App.jsx
+++ Levro_service/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import Navbar from './components/Navbar';
+import AboutUs from './Pages/Aboutus';
 
 function App() {
   return (
    <div className="App">
     <Navbar />
+    <AboutUs />
    </div>
   );
 }
