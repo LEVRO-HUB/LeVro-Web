f3efb5be7e3aa97e8958e9ca85f87d07498e0fbe
HARIHARAN26424 <146554348+HARIHARAN26424@users.noreply.github.com>
2026-03-02 22:49:52 +0530

    Merge levro_hari into main - favicon fix

--- Levro_service/src/App.jsx
+++ Levro_service/src/App.jsx
@@ -2,6 +2,7 @@ import ContactUs from './Pages/ContactUs';
 import './App.css';
 import Home from './Pages/Home';
 import Service from './Pages/Service';
+
 import About from './Pages/About';
 import Careers from './Pages/Careers';
 import Footer from './components/Footer';
@@ -12,7 +13,7 @@ import ScrollToHash from "./components/ScrollToHash";
 
 import AboutUs from './Pages/Aboutus';
 
-function App() {
+function Mainpage() {
   return ( 
     <>
       <section id="home"><Home /></section>
